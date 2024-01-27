1756e328205a6c22e0714688eb69a271c1ffc524
Xymion <nilla.yliluoma@gmail.com>
2024-01-27 16:11:40 +0900

    Scenes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject pauseScreen;
+    public GameObject gameOverScreen;
 
     // Start is called before the first frame update
     void Start()
