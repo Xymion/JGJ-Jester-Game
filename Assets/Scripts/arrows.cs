ec1dfe84b100d8eaaf076242796e6ede670e65fa
Alexandre <alexandre.santacreu@epita.fr>
2024-01-27 17:37:47 +0900

    workinggameplay

--- Assets/Scripts/arrows.cs
+++ Assets/Scripts/arrows.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 using Random = System.Random;
 
@@ -13,32 +14,170 @@ public class arrows : MonoBehaviour
     public List<GameObject> comingmoves = new List<GameObject>();
     private Random rand = new Random();
     private float[] arrowspositions = { 4, 5.5f, 7, 8.5f };
-    public GameObject ScoreTextMeshPro;
+    public TextMeshProUGUI ScoreTextMeshPro;
+    public int scorevalue = 0;
+    public float horizontalInput;
+    public float verticalInput;
+    private float arrowmaxpos = 2.5f;
+    public TextMeshProUGUI arrLeftTextMeshPro;
+    public TextMeshProUGUI arrRightTextMeshPro;
+    public TextMeshProUGUI arrUpTextMeshPro;
+    public TextMeshProUGUI arrDownTextMeshPro;
+    public TextMeshProUGUI missTextMeshPro;
+    public TextMeshProUGUI perfectTextMeshPro;
+    public TextMeshProUGUI almostTextMeshPro;
+    public int misscount = 0;
+    public int perfectcount = 0;
+    public int almostcount = 0;
+    float delayforarr = 0;
+    public float delayconstant = 0.5f;
+
 
     void Start()
     {
-        comingmoves.Add(Instantiate(circlepref, new Vector3(4, 0, 0), Quaternion.identity));
-        comingmoves.Add(Instantiate(circlepref, new Vector3(5.5f, -1, 0), Quaternion.identity));
-        comingmoves.Add(Instantiate(circlepref, new Vector3(7, -2, 0), Quaternion.identity));
-        comingmoves.Add(Instantiate(circlepref, new Vector3(8.5f, -3, 0), Quaternion.identity));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (comingmoves.Count < 2)
+        //delayconstant = Math.Max(0.3f, delayconstant - 0.00001f);
+        delayforarr = Math.Max(0, delayforarr - Time.deltaTime);
+        arrUpTextMeshPro.fontSize = 36;
+        arrDownTextMeshPro.fontSize = 36;
+        arrLeftTextMeshPro.fontSize = 36;
+        arrRightTextMeshPro.fontSize = 36;
+        //scorevalue++;
+        ScoreTextMeshPro.text = "Score : " + scorevalue.ToString();
+        if (delayforarr == 0)
         {
-            comingmoves.Add(Instantiate(circlepref, new Vector3(arrowspositions[rand.Next(0,3)], -3, 0), Quaternion.identity));
+            delayforarr = delayconstant;
+            comingmoves.Add(Instantiate(circlepref, new Vector3(arrowspositions[rand.Next(0,3)], -6, 0), Quaternion.identity));
         }
+        
         foreach (var el in comingmoves)
         {
             var pos = el.transform.position;
-            if (pos.y > 2.5)
+            if (pos.y > arrowmaxpos)
             {
+                misscount++;
+                missTextMeshPro.text = "Misses : " + misscount.ToString();
                 comingmoves.Remove(el);
                 Destroy(el);
+                //scorevalue -= 50;
             }
-            el.transform.position = new Vector3(pos.x, (float)(pos.y + 0.01), pos.z);
+            else
+            {
+                el.transform.position = new Vector3(pos.x, (float)(pos.y + 0.01), pos.z);
+            }
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        InputManager();
+        
+        horizontalInput = 0;
+        verticalInput = 0;
+    }
+
+    private void InputManager()
+    {
+        if (verticalInput > 0)
+        {
+            arrUpTextMeshPro.fontSize = 50;
+            var arr = comingmoves[0];
+            if (comingmoves[0].transform.position.x == 7)
+            {
+                if (arrowmaxpos - arr.transform.position.y < 0.5) //perfect timing
+                {
+                    scorevalue += 50;
+                    comingmoves.Remove(arr);
+                    Destroy(arr);
+                    perfectcount++;
+                    perfectTextMeshPro.text = "Perfect : " + perfectcount.ToString();
+                }
+                else if (arrowmaxpos - arr.transform.position.y < 1) //bad timing but still close
+                {
+                    scorevalue += (int) (50 * (1 - (arrowmaxpos - arr.transform.position.y)));
+                    comingmoves.Remove(arr);
+                    Destroy(arr);
+                    almostcount++;
+                    almostTextMeshPro.text = "Almost : " + almostcount.ToString();
+                }
+            }
+        }
+        if (verticalInput < 0)
+        {
+            arrDownTextMeshPro.fontSize = 50;
+            var arr = comingmoves[0];
+            if (comingmoves[0].transform.position.x == 5.5f)
+            {
+                if (arrowmaxpos - arr.transform.position.y < 0.3) //perfect timing
+                {
+                    scorevalue += 50;
+                    comingmoves.Remove(arr);
+                    Destroy(arr);
+                    perfectcount++;
+                    perfectTextMeshPro.text = "Perfect : " + perfectcount.ToString();
+                }
+                else if (arrowmaxpos - arr.transform.position.y < 0.6) //bad timing but still close
+                {
+                    scorevalue += (int) (50 * (1 - (arrowmaxpos - arr.transform.position.y)));
+                    comingmoves.Remove(arr);
+                    Destroy(arr);
+                    almostcount++;
+                    almostTextMeshPro.text = "Almost : " + almostcount.ToString();
+                }
+            }
+        }
+        if (horizontalInput > 0)
+        {
+            arrRightTextMeshPro.fontSize = 50;
+            var arr = comingmoves[0];
+            if (comingmoves[0].transform.position.x == 8.5f)
+            {
+                if (arrowmaxpos - arr.transform.position.y < 0.3) //perfect timing
+                {
+                    scorevalue += 50;
+                    comingmoves.Remove(arr);
+                    Destroy(arr);
+                    perfectcount++;
+                    perfectTextMeshPro.text = "Perfect : " + perfectcount.ToString();
+                }
+                else if (arrowmaxpos - arr.transform.position.y < 0.6) //bad timing but still close
+                {
+                    scorevalue += (int) (50 * (1 - (arrowmaxpos - arr.transform.position.y)));
+                    comingmoves.Remove(arr);
+                    Destroy(arr);
+                    almostcount++;
+                    almostTextMeshPro.text = "Almost : " + almostcount.ToString();
+                }
+            }
+        }
+        if (horizontalInput < 0)
+        {
+            arrLeftTextMeshPro.fontSize = 50;
+            var arr = comingmoves[0];
+            if (comingmoves[0].transform.position.x == 4)
+            {
+                if (arrowmaxpos - arr.transform.position.y < 0.3) //perfect timing
+                {
+                    scorevalue += 50;
+                    comingmoves.Remove(arr);
+                    Destroy(arr);
+                    perfectcount++;
+                    perfectTextMeshPro.text = "Perfect : " + perfectcount.ToString();
+                }
+                else if (arrowmaxpos - arr.transform.position.y < 0.6) //bad timing but still close
+                {
+                    scorevalue += (int) (50 * (1 - (arrowmaxpos - arr.transform.position.y)));
+                    comingmoves.Remove(arr);
+                    Destroy(arr);
+                    almostcount++;
+                    almostTextMeshPro.text = "Almost : " + almostcount.ToString();
+                }
+            }
         }
     }
 }
