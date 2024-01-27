eb03f9161cd11938386d581bc01d6be6bec45ff2
Xymion <nilla.yliluoma@gmail.com>
2024-01-27 23:50:12 +0900

    Rythym

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject pauseScreen;
     public GameObject gameOverScreen;
+    public int currentScore;
+    public int scorePerNote = 100;
+
+    public TextMeshProUGUI scoreText;
+
+    public NoteScroller NS;
+
+    public static GameManager instance;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance= this;
+        scoreText.text = "0";
     }
 
     // Update is called once per frame
@@ -30,5 +41,17 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0;
     }
 
+    public void NoteHit()
+    {
+        Debug.Log("Hit On Time");
+
+        currentScore += scorePerNote;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void NoteMiss()
+    {
+        Debug.Log("MISS");
+    }
     
 }
